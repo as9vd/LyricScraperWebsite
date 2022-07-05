ddcbcc713d66cae970c248c2ce25d4d8fe95591c
as-4030 <as9vd@virginia.edu>
2022-07-05 11:27:28 -0400

    Implemented Spring REST.

--- src/main/java/com/asad/geniusanalysis/repository/SongRepository.java
+++ src/main/java/com/asad/geniusanalysis/repository/SongRepository.java
@@ -2,7 +2,9 @@ package com.asad.geniusanalysis.repository;
 
 import com.asad.geniusanalysis.entity.Song;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
+@CrossOrigin("http://localhost:4200")
 public interface SongRepository extends JpaRepository<Song, Integer> {
 
 }
